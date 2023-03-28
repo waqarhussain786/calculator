daeb483eeeeda58892aede6cbf43de5dc9c32709
waqarhussain786 <wh751212@gmail.com>
2023-03-28 19:16:40 +0500

    a

--- java.js
+++ java.js
@@ -15,7 +15,6 @@ arr.forEach(button => {
             input.value = string;
         }
         else if (e.target.innerHTML == 'DEL') {
-            console.log("del");
             string = string.substring(0, string.length - 1);
             input.value = string;
         }
